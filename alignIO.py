9ce77f74ff0d76bf9354ab86c2a47d3de13b3603
CedricVerl <cedric.verlinden@hotmail.com>
2022-12-04 15:03:27 +0100

    yes

--- alignIO.py
+++
@@ -1,6 +0,0 @@
-def alignIO(out, sync, Nr):
-    # Out: dataStream
-    # pulse: sync pulse
-    # Nr: amount of zero padding samples between pulse and data stream
-    # xcorr = np.xcorr(out, sync)
-    pass
